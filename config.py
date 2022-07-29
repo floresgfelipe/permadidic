15bf4a8331f0731d3ab16a43724b6797c3d5941f
Felipe Flores <floresgfelipealberto@outlook.com>
2022-07-29 12:21:15 -0500

    Init DB

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ class Config(object):
     RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY')
     RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY')
     RECAPTCHA_PARAMETERS = {'hl':'es'}
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
