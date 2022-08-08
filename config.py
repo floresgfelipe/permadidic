2aa6f21190356c68ee6e5e8a8f7d013bd6924ec6
Felipe Flores <floresgfelipealberto@outlook.com>
2022-08-08 12:58:47 -0500

    config uploads

--- config.py
+++ config.py
@@ -11,4 +11,7 @@ class Config(object):
     RECAPTCHA_PARAMETERS = {'hl':'es'}
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAX_CONTENT_LENGTH = 5 * 1024 * 1024
+    UPLOAD_EXTENSIONS = ['.jpg', '.png', '.gif', '.jpeg', '.pdf']
+    UPLOAD_PATH = 'u'
     
